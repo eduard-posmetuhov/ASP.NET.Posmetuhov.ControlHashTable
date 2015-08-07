7fa7448f611ffce4bac831de469d46c731821bb6
eduard-posmetuhov <eduard.posmetuhov@gmail.com>
2015-08-07 09:12:52 +0200

    HashTable

---
+++ HashTableLib/HashTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableLib
+{
+    public class HashTable<T>
+    {
+        List<Pairs<T>> storage;
+
+        public HashTable()
+        {
+            storage = new List<Pairs<T>>();
+        }
+
+        public HashTable(params T[]arrayValue)
+        {
+            storage = new List<Pairs<T>>();
+            foreach (var item in arrayValue)
+                Add(item);
+        }
+
+        public void Add(T value)
+        {
+            if (!this.ContainsValue(value))
+            storage.Add(new Pairs<T>(value));            
+        }
+
+        public bool ContainsKey(int key)
+        {
+            foreach (var item in storage)
+            {
+                if (item.GetValue().GetHashCode() == key)
+                    return true;
+            }
+            return false;
+        }
+
+        public bool ContainsValue(T value)
+        {
+            foreach (var item in storage)
+            {
+                if (item.GetValue().Equals(value))
+                    return true;
+            }
+            return false;
+        }
+
+
+
+    }
+}
---
+++ HashTableLib/Pairs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableLib
+{
+    public class Pairs<T>
+    {
+        int key;
+        T value ;
+
+        public Pairs(T value)
+        {
+            this.value = value;
+            this.key = value.GetHashCode();
+        }
+
+        public T GetValue()
+        {
+            return value;
+        }
+
+    }
+}
